629ef6f9f261df7e3bf1685baea4c2cdeb081295
Gabriel <bhcmonteiro54@gmail.com>
2023-09-24 20:44:38 -0300

    Requisito do post /sign-up feito

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 import express from 'express';
+import cors from 'cors';
 
 //Aplicação servidora
 const app = express();
 
 //Configurações
-app.use(cors);
+app.use(cors());
 app.use(express.json());
+
+//Variáveis globais
+const users =[];
+const tweets = [];
+
+//Funcões
+app.post('/sign-up', (request, response) => {
+
+    const {username, avatar} = request.body;
+
+    const novoUser = {username, avatar};
+
+    users.push(novoUser);
+
+    console.log(users);
+
+    response.send('ok!');
+})
+app.get("/test", (request, response) => {
+    response.send("Tudo ok");
+})
 
 //Executando a aplicação servidora para requisições na porta 5000
 const PORT = 5000;
